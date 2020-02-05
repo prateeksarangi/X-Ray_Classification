9a0dae44f2320bf4e6b0fc37d82f571363714fc0
subrat1729 <subratpatnaik.email@gmail.com>
2020-02-05 17:05:23 +0530

    Added dataset in chest_xray folder and replaced direct path with dynamic path.

--- ServerSide.py
+++ ServerSide.py
@@ -1,6 +1,7 @@
 from keras.models import model_from_json
 from keras.preprocessing import image
 import numpy as np
+import os
 
 with open("model.json") as json_file:
     model = model_from_json(json_file.read())
@@ -10,9 +11,7 @@ img_width, img_height = 150, 150
 
 model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
 
-img = image.load_img(
-    "/Users/ashwini/Desktop/chest_xray/chest_xray/test/PNEUMONIA/person1616_virus_2802.jpeg",
-                target_size = (img_width, img_height))
+img = image.load_img(os.getcwd()+"/chest_xray/test/PNEUMONIA/person1_virus_7.jpeg", target_size = (img_width, img_height))
 
 x = image.img_to_array(img)
 x = np.expand_dims(x, axis = 0)
