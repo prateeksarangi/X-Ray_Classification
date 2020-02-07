ba26b6dd9a339e1392c8a9cb2ed589bfaedfa189
subrat1729 <subratpatnaik.email@gmail.com>
2020-02-07 15:49:25 +0530

    changes in exec.py

--- ServerSide.py
+++ ServerSide.py
@@ -3,20 +3,24 @@ from keras.preprocessing import image
 import numpy as np
 import os
 
-with open("Model/model.json") as json_file:
-    model = model_from_json(json_file.read())
-    model.load_weights("Model/weight.h5")
+def predict_class(path):
+    with open("Model/model.json") as json_file:
+        model = model_from_json(json_file.read())
+        model.load_weights("Model/weight.h5")
 
-img_width, img_height = 150, 150
+    img_width, img_height = 150, 150
 
-model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+    model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
 
-img = image.load_img(os.getcwd()+"/chest_xray/test/PNEUMONIA/person1_virus_7.jpeg", target_size = (img_width, img_height))
+    img = image.load_img(path, target_size = (img_width, img_height))
 
-x = image.img_to_array(img)
-x = np.expand_dims(x, axis = 0)
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis = 0)
 
-images = np.vstack([x])
-classes = model.predict_classes(images, batch_size = 1)
+    images = np.vstack([x])
+    classes = model.predict_classes(images, batch_size = 1)
 
-print(classes)
+    print(classes)
+    return classes
+
+predict_class(os.getcwd()+'/chest_xray/test/NORMAL/IM-0001-0001.jpeg')
--- exec.py
+++ exec.py
@@ -1,11 +1,23 @@
+import os
+
 from flask import *
+from ServerSide import predict_class
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    """if request.method == 'POST':
-        file = request.files['inputFile']
-        return file.filename"""
     return render_template('index.html')
+
+@app.route('/', methods = ['POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['inputFile']
+        path = os.getcwd()+'/uploads/'+file.filename
+        file.save(path)
+        print('PATH: {}'.format(path))
+        output=predict_class(path)
+        print('OUTPUT: {}'.format(output))
+        return render_template('index.html',predict=output)
 
 app.run(debug=True)
