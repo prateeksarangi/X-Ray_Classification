5a44288b05a51681e5d4621006d81930ea4f8e7f
Subrat Patnaik <subratpatnaik.email@gmail.com>
2020-02-10 23:38:30 +0530

    Add files via upload

--- exec.py
+++ exec.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
-class User(db.Model):
+class Patient(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     pname = db.Column(db.String(20), nullable=False)
     gender = db.Column(db.String(10), nullable=False)
@@ -16,15 +16,25 @@ class User(db.Model):
     result=db.Column(db.String(20),nullable=False)
 
     def __repr__(self):
-        return f"User('{self.pname}','{self.gender}','{self.age}','{self.pno}','{self.result}')"
+        return f"Patient('{self.pname}','{self.gender}','{self.age}','{self.pno}','{self.result}')"
+
+class Prescription(db.Model):
+    prid = db.Column(db.Integer, primary_key=True)
+    annot = db.Column(db.String(200), nullable=False)
+    did = db.Column(db.Integer, db.ForeignKey('doctor.did'))
+    doctor = db.relationship('Doctor', backref=db.backref('doctor', uselist=False))
+    pid = db.Column(db.Integer, db.ForeignKey('patient.id'))
+    patient = db.relationship('Patient', backref=db.backref('patient', uselist=False))
+
+
 class Doctor(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
     dname = db.Column(db.String(20), nullable=False)
-    did = db.Column(db.Integer, nullable=False)
+    did = db.Column(db.Integer, primary_key=True)
     dno = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
         return f"Doctor('{self.dname}', '{self.did}', '{self.dno}')"
+
 db.create_all()
 
 @app.route('/')
@@ -43,13 +53,13 @@ def upload_file():
             labelans = "Normal"
         else:
             labelans = "Pneumonia"
-        user1 = User(pname=request.form['name'], gender=request.form['gender'], age=request.form['age'], pno=request.form['pno'], result=labelans)
-        db.session.add(user1)
+        patient1 = Patient(pname=request.form['name'], gender=request.form['gender'], age=request.form['age'], pno=request.form['pno'], result=labelans)
+        db.session.add(patient1)
         db.session.commit()
         doctor1 = Doctor(dname=request.form['d_name'], did=request.form['d_id'], dno=request.form['d_mob'])
         db.session.add(doctor1)
         db.session.commit()
-        return render_template('index.html',predict=labelans,image_file=image_file,pname=user1.pname,gender=user1.gender,age=user1.age,pno=user1.pno,dname=doctor1.dname,dno=doctor1.dno,did=doctor1.did)
+        return render_template('index.html',predict=labelans,image_file=image_file,pname=patient1.pname,gender=patient1.gender,age=patient1.age,pno=patient1.pno,dname=doctor1.dname,dno=doctor1.dno,did=doctor1.did)
 
 if __name__ == '__main__':
     app.run(debug=True)
