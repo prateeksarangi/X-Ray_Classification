f2885d1dff8c81f4c762033ca838a2f5048c5476
Subrat Patnaik <subratpatnaik.email@gmail.com>
2020-02-11 01:48:50 +0530

    Add files via upload

--- exec.py
+++ exec.py
@@ -1,11 +1,16 @@
 import os
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template, request, url_for, flash
+from werkzeug.utils import redirect
+
 from ServerSide import predict_class
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+
+doctor_id = 0
+patient_id = 0
 
 class Patient(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -16,7 +21,7 @@ class Patient(db.Model):
     result=db.Column(db.String(20),nullable=False)
 
     def __repr__(self):
-        return f"Patient('{self.pname}','{self.gender}','{self.age}','{self.pno}','{self.result}')"
+        return f"Patient(id='{self.id}',name='{self.pname}',gender='{self.gender}',age='{self.age}',phone='{self.pno}',result='{self.result}')"
 
 class Prescription(db.Model):
     prid = db.Column(db.Integer, primary_key=True)
@@ -26,6 +31,8 @@ class Prescription(db.Model):
     pid = db.Column(db.Integer, db.ForeignKey('patient.id'))
     patient = db.relationship('Patient', backref=db.backref('patient', uselist=False))
 
+    def __repr__(self):
+        return f"Prescription(id='{self.prid}',annotation='{self.annot}',p_id='{self.pid}',d_id='{self.did}')"
 
 class Doctor(db.Model):
     dname = db.Column(db.String(20), nullable=False)
@@ -33,16 +40,19 @@ class Doctor(db.Model):
     dno = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
-        return f"Doctor('{self.dname}', '{self.did}', '{self.dno}')"
+        return f"Doctor(name='{self.dname}', id='{self.did}', phone='{self.dno}')"
 
 db.create_all()
 
 @app.route('/')
 def index():
+    global doctor_id, patient_id
+    doctor_id = 0
+    patient_id = 0
     return render_template('index.html')
 
 @app.route('/', methods = ['GET', 'POST'])
-def upload_file():
+def predict_and_upload():
     if request.method == 'POST':
         file = request.files['images']
         path = os.getcwd()+'/static/tempdata/'+file.filename
@@ -54,13 +64,24 @@ def upload_file():
         else:
             labelans = "Pneumonia"
         patient1 = Patient(pname=request.form['name'], gender=request.form['gender'], age=request.form['age'], pno=request.form['pno'], result=labelans)
+        doctor1 = Doctor(dname=request.form['d_name'], did=request.form['d_id'], dno=request.form['d_mob'])
         db.session.add(patient1)
         db.session.commit()
-        doctor1 = Doctor(dname=request.form['d_name'], did=request.form['d_id'], dno=request.form['d_mob'])
+        global patient_id, doctor_id
+        patient_id = (Patient.query.all())[-1].id
         db.session.add(doctor1)
         db.session.commit()
+        doctor_id = doctor1.did
         return render_template('index.html',predict=labelans,image_file=image_file,pname=patient1.pname,gender=patient1.gender,age=patient1.age,pno=patient1.pno,dname=doctor1.dname,dno=doctor1.dno,did=doctor1.did)
+
+@app.route('/upload', methods = ['GET', 'POST'])
+def upload_annot():
+    if request.method=='POST':
+        global doctor_id, patient_id
+        prescription1 = Prescription(annot=request.form['annotation'],pid=patient_id,did=doctor_id)
+        db.session.add(prescription1)
+        db.session.commit()
+        return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
-
